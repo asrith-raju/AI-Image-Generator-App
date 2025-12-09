28e9eb80fe3486e750deb0eeaaa31c8e75589b23
asrith-raju <asrithraju143@gmail.com>
2025-12-09 18:52:20 +0530

    client(feat): add backend url in client

--- client/src/context/Appcontext.jsx
+++ client/src/context/Appcontext.jsx
@@ -4,12 +4,16 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props)=>{
     const [user, setuser] = useState(null)
-
     const [showLogin, setshowLogin] = useState(false)
     
+
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    
     const value={
-        user,setuser,showLogin,setshowLogin
+        user,setuser,showLogin,setshowLogin, backendUrl
     }
+
     return(
         <AppContext.Provider value={value}>
            {props.children}
